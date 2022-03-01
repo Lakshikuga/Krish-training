83f43125c976675e55addec027899ae9da01cd5c
LakshiKuga <lakkuga@gmail.com>
2022-03-01 11:35:26 +0530

    Adding solution

---
+++ WickysProblem/Jump.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Jump {
+	public static void main(String args[]) {
+
+		Scanner input  = new Scanner(System.in);
+		
+		//INPUT THE DISTANCE NEED TO TRAVEL
+		System.out.println("How far do you expect Wicky to jump?");
+		int distance = input.nextInt();
+		
+		int distance_jumped = 0, time_taken_to_jump = 0;
+
+		while(distance > distance_jumped) {
+			distance_jumped+=5;
+			if(distance_jumped >= distance) {
+				System.out.println("He jumped for " + time_taken_to_jump +"s");
+				System.out.println("He jumped " + distance_jumped +"m");
+				break;
+			}
+			else{
+				time_taken_to_jump+=1;
+				distance_jumped+=3;
+			}
+			
+			if(distance_jumped > distance) {
+				System.out.println("He jumped for " + time_taken_to_jump +"s");
+				System.out.println("He jumped " + distance_jumped +"m");
+				break;
+			}
+			
+			else {
+				time_taken_to_jump+=2;
+				distance_jumped+=1;
+			}
+
+			if(distance_jumped > distance) {
+				System.out.println("He jumped for " + time_taken_to_jump +"s");
+				System.out.println("He jumped " + distance_jumped +"m");
+				break;
+			}
+
+			else{
+				time_taken_to_jump+=3; //distance_jumped=18m
+			}
+			
+		}
+	}
+}
+				
+
+				
+			
+			
+
+		
+
+			
+			
+
+		
+
+		
