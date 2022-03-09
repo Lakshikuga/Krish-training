bc0cb4a2f1832804b6efb97311c47652f5a50097
LakshiKuga <lakkuga@gmail.com>
2022-03-09 14:02:51 +0530

    modified logic in code

--- CCH_NoOfOccurences/src/NoOfOccurrences.java
+++ CCH_NoOfOccurences/src/NoOfOccurrences.java
@@ -19,31 +19,26 @@ public class NoOfOccurrences {
         for(int i=0; i<wordArr.length; i++){
             count = 1;
             for (int j=i+1; j<wordArr.length; j++) {
-                if(Character.isUpperCase(wordArr[i])) {
-                    if(Character.toLowerCase(wordArr[i]) == wordArr[j]) {
-                        count++;
-                        wordArr[j] = '0';
-                    }
-                }
-                if(Character.isUpperCase(wordArr[j])){
-                    if(wordArr[i] == Character.toLowerCase(wordArr[j])) {
-                        count++;
-                        wordArr[j] = '0';
-                    }
-                }
-                else if(!Character.isUpperCase(wordArr[i]) && !Character.isUpperCase(wordArr[j])) {
+                if ((Character.isUpperCase(wordArr[i]) && Character.isUpperCase(wordArr[j])) || (!Character.isUpperCase(wordArr[i]) && !Character.isUpperCase(wordArr[j]))) {
                     if (wordArr[i] == wordArr[j]) {
                         count++;
                         wordArr[j] = '0';
                     }
                 }
-                else if (Character.isUpperCase(wordArr[i]) && Character.isUpperCase(wordArr[j])) {
-                        if (wordArr[i] == wordArr[j]) {
-                            count++;
-                            wordArr[j] = '0';
-                        }
+                else if(Character.isUpperCase(wordArr[i])) {
+                    if(Character.toLowerCase(wordArr[i]) == wordArr[j]) {
+                        count++;
+                        wordArr[j] = '0';
                     }
                 }
+                else if(Character.isUpperCase(wordArr[j])){
+                    if(wordArr[i] == Character.toLowerCase(wordArr[j])) {
+                        count++;
+                        wordArr[j] = '0';
+                    }
+                }
+
+            }
             if(count>=1 && wordArr[i]!='0') {
                 System.out.println(wordArr[i] + " " + "-" + "" + count);
 
