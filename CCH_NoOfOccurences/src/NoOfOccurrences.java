2b7202bcc95fe8523bc8c8aa938fdef7be11c9fb
LakshiKuga <lakkuga@gmail.com>
2022-03-09 14:24:26 +0530

    adding comments

--- CCH_NoOfOccurences/src/NoOfOccurrences.java
+++ CCH_NoOfOccurences/src/NoOfOccurrences.java
@@ -19,18 +19,22 @@ public class NoOfOccurrences {
         for(int i=0; i<wordArr.length; i++){
             count = 1;
             for (int j=i+1; j<wordArr.length; j++) {
+                //when both letters are capital or simple
                 if ((Character.isUpperCase(wordArr[i]) && Character.isUpperCase(wordArr[j])) || (!Character.isUpperCase(wordArr[i]) && !Character.isUpperCase(wordArr[j]))) {
                     if (wordArr[i] == wordArr[j]) {
                         count++;
                         wordArr[j] = '0';
                     }
                 }
+                //make a capital letter to lowercase and compare that
                 else if(Character.isUpperCase(wordArr[i])) {
                     if(Character.toLowerCase(wordArr[i]) == wordArr[j]) {
                         count++;
+                        //to prevent the repeated character from being counted again so flagging as visited for that character.
                         wordArr[j] = '0';
                     }
                 }
+                //make a capital letter to lowercase and compare that
                 else if(Character.isUpperCase(wordArr[j])){
                     if(wordArr[i] == Character.toLowerCase(wordArr[j])) {
                         count++;
@@ -39,6 +43,7 @@ public class NoOfOccurrences {
                 }
 
             }
+            //printing only the characters that has been visited and not printing those that were revisited!
             if(count>=1 && wordArr[i]!='0') {
                 System.out.println(wordArr[i] + " " + "-" + "" + count);
 
