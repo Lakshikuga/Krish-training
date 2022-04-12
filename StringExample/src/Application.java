15ccf7e2ac500d1a546e473c48e785e563cf4854
LakshiKuga <lakkuga@gmail.com>
2022-04-12 08:37:02 +0530

    Adding string example

--- StringExample/src/Application.java
+++ StringExample/src/Application.java
@@ -4,17 +4,39 @@ public class Application {
 
         String name = "John";
         String person = "John";
-        String employee = "John";
+        //String employee = "John";
+
+        String employee = new String("John");
 
         //using the == sign
         System.out.println(name==person); //true
-        System.out.println(person==employee); //true
-        System.out.println(name==employee); //true
+        System.out.println(person==employee); //false
+        System.out.println(name==employee); //false
 
         System.out.println();
         //using the .equals
         System.out.println(name.equals(person)); //true
         System.out.println(person.equals(employee)); //true
         System.out.println(name.equals(employee)); //true
+
+        System.out.println();
+        
+        String str1 = "Python";
+        String str2 = "Data Science";
+        String str3 = "Python";
+
+        String str4 = new String("Java");
+        String str5 = new String("Python");
+
+        //using the == sign
+        System.out.println(str1==str3); //true
+        System.out.println(str3==str5); //false
+
+
+        System.out.println();
+        //using the .equals
+        System.out.println(str1.equals(str3)); //true
+        System.out.println(str1.equals(str5)); //true
+
     }
 }
