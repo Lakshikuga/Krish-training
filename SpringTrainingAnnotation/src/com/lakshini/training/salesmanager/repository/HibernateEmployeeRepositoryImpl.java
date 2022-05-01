5b0fa80d1a5b8b824f48b78edb462b3f951742ba
LakshiKuga <lakkuga@gmail.com>
2022-05-01 12:47:11 +0530

    updating SpringTrainingAnnotation

--- SpringTrainingAnnotation/src/com/lakshini/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
+++ SpringTrainingAnnotation/src/com/lakshini/training/salesmanager/repository/HibernateEmployeeRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.lakshini.training.salesmanager.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.lakshini.training.salesmanager.model.Employee;
@@ -10,13 +11,26 @@ import com.lakshini.training.salesmanager.model.Employee;
 @Repository("employeeRepository")
 public class HibernateEmployeeRepositoryImpl implements EmployeeRepository {
 	
+	//Reading values from application.properties
+	@Value("${name}")
+	private String employeeName;
+	@Value("${city}")
+	private String employeeLocation;
+	
 	public List<Employee> getAllEmployees() {
 		
 		List<Employee> employees = new ArrayList<Employee>();
 		
 		Employee employee = new Employee();
+		
+		employee.setEmployeeName(employeeName);
+		employee.setEmployeeLocation(employeeLocation);
+		
+		
+		/*
 		employee.setEmployeeName("Lakshi");
 		employee.setEmployeeLocation("Dehiwela");
+		*/
 		
 		employees.add(employee); //adding to array list.
 		
