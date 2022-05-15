016840c6f4c3203c4448bf712e00d8727722a30b
LakshiKuga <lakkuga@gmail.com>
2022-05-15 20:39:52 +0530

    second commit

--- DateTimeAPI/src/DateTimeDemo.java
+++ DateTimeAPI/src/DateTimeDemo.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class DateTimeDemo {
 
@@ -42,13 +43,25 @@ public class DateTimeDemo {
         int dddt = dt.getDayOfMonth();
         int mmdt = dt.getMonthValue();
         int yyyydt = dt.getYear();
+        System.out.printf("%d-%d-%d", dddt, mmdt, yyyydt);
 
         int hh = dt.getHour();
         int mindt = dt.getMinute();
         int secdt = dt.getSecond();
         int nanodt = dt.getNano();
+        System.out.printf("\n%d:%d:%d:%d", hh, mindt, secdt, nanodt);
 
-        
+        //Representing a particular date and time
+        LocalDateTime dateTime = LocalDateTime.of(1995, 5, 28, 12, 45);
+        LocalDateTime dateTime1 = LocalDateTime.of(1994, Month.OCTOBER, 20, 3, 45);
+
+        System.out.println(dateTime);
+        System.out.println(dateTime1);
+
+        //LocalDateTime class has multiple methods to find the date and time of the day after a certain period.
+        System.out.println("After 6 months : "+dt.plusMonths(6));
+        System.out.println("Before 6 months : "+dt.minusMonths(6));
+
 
 
     }
